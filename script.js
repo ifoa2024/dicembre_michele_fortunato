bd6c5b1e2952c186f8f2962d6007b5a63b4bfed8
pacedom9 <pacedomenico73@gmail.com>
2024-12-16 11:03:05 +0100

    salve

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
-export {persona, sum} from "./function.js"
 
 
-const persona = {
+
+ export const persona = {
     nome: 'alice',
     età: 23,
     city: 'new york',
@@ -12,7 +12,31 @@ const  {nome, età, city} = persona;
 console.log(persona)
 
 
-function sum(n1, n2) {
+export function sum(n1, n2) {
     sum = n1+n2;
     return sum;
 }
+
+console.log(sum());
+
+
+
+let options = {
+    size: {
+        width: 100,
+        heigth: 200,
+    },
+    items: ['cake', 'donut'],
+    extra: true,
+}
+
+let {size:{width, heigth}, items: [cake, donut], extra} = options;
+alert(width);
+
+
+function printNameAge() {
+    console.log(`${nome}, anni ${età}`);
+    
+}
+
+console.log(printNameAge())
